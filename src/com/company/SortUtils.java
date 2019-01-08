6aa585bf4c235932e9045784b789317c477e0cd9
tbelal2209 <43207470+tbelal2209@users.noreply.github.com>
2019-01-08 08:43:59 -0500

    Update SortUtils.java

--- src/com/company/SortUtils.java
+++ src/com/company/SortUtils.java
@@ -1,12 +1,24 @@
 package com.company;
 
 public class SortUtils {
-    public static boolean isSorted(int[] testArr) {
+    int[] testArr = {8,6,7,5,3,0,9,10,1,2,3};
+    public static int isSorted(int[] testArr) {
+        int min = testArr[0];
         for (int i = 0; i < testArr.length; i++) {
-            if ((testArr[i] + testArr[i - 1])!= (8+6+7+5+3+0+9+10+1+2+3)){
+            if (i<min){
+              return testArr[i];
+            }
+        }
+        return min;
+    }
+    public static boolean checkSum(int[] testArr){
+        for (int i = 0; i<testArr.length;i++){
+            if(testArr[i]+testArr[i-1]!=){
 
             }
         }
-        return false;
     }
+
+
 }
+
