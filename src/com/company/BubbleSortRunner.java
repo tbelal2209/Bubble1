1e640658e4533e891d1c0d06b8c168c34f6a90fa
tbelal2209 <43207470+tbelal2209@users.noreply.github.com>
2019-01-13 17:05:25 -0500

    Update BubbleSortRunner.java

--- src/com/company/BubbleSortRunner.java
+++ src/com/company/BubbleSortRunner.java
@@ -3,15 +3,17 @@ package com.company;
 public class BubbleSortRunner {
     public static void main(String[] args) {
         int[] testArr = {8, 6, 7, 5, 3, 0, 9, 10, 1, 2, 3};
-        int[] arr = {1,2,3,5,4};
+        String[] arr = {String.valueOf(1),String.valueOf(2),String.valueOf(3),String.valueOf(4), String.valueOf(5)};
         System.out.print("Before: ");
         for (int num : testArr)
             System.out.print(num + " ");
         System.out.println();
-        SortUtils.isSorted(testArr);
+        SortingUtils.isSorted(testArr);
         BubbleSort.bubbleSort(testArr);
-        SortUtils.checkSum(testArr);
-        SelectionSort.insertionSort(arr);
+        SortingUtils.checkSum(testArr);
+        SelectionSort.copyArr(arr);
+        SelectionSort.copydoubleArr(arr);
+        SelectionSort.copylist(arr);
         System.out.print("After: ");
         for (int num : testArr)
             System.out.print(num + " ");
