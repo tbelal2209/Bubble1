556dbb391d9a251c35965ce9a2b5d431035f0231
tbelal2209 <43207470+tbelal2209@users.noreply.github.com>
2019-01-13 17:06:29 -0500

    Update SelectionSort.java

--- src/com/company/SelectionSort.java
+++ src/com/company/SelectionSort.java
@@ -1,11 +1,29 @@
 package com.company;
 
 public class SelectionSort {
-    public static void insertionSort(int[] arr){
-        int arrstorage, backwardscount;
-        for(int i=0;i< arr.length;i++){
-            arrstorage = arr[i];
-            while()
+    public static int[] copyArr(String[] testArr) {
+        int[] copytestArr = new int[10];
+        for (int n = 0; n < testArr.length; n++) {
+            copytestArr[n] = Integer.parseInt(testArr[n]);
         }
+        return copytestArr;
     }
+
+    public static String[] copylist(String[] list) {
+        String[] copylist = new String[10];
+        for (int n = 0; n < list.length; n++) {
+            copylist[n] = list[n];
+        }
+        return copylist;
+    }
+
+    public static double[] copydoubleArr(String[] doubleArr) {
+        double[] copydoubleArr = new double[10];
+        for (int n = 0; n < doubleArr.length; n++) {
+            copydoubleArr[n] = Double.parseDouble(doubleArr[n]);
+        }
+        return copydoubleArr;
+    }
+
+
 }
